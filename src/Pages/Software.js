a92931a0dad525d3c192019fc3d04385b25e7292
felice <feliceho006@gmail.com>
2022-02-09 21:48:30 +0800

    Updates

--- src/Pages/Software.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Software() {
-  return (<div></div>)
-}
-
-export default Software;
--- src/Pages/VisualArts.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function VisualArts() {
-  return <div></div>;
-}
-
-export default VisualArts;
